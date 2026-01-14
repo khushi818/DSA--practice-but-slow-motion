07495d42530da7977031623d5e97f142e48c90c8
khushi <khushishukla5890@gmail.com>
2026-01-14 06:51:28 +0530

    LCM

--- Number_based_Coding_questions/GCD-LCM.js
+++ Number_based_Coding_questions/GCD-LCM.js
@@ -51,3 +51,52 @@ function findGCD(a, b) {
   }
 
 //   O(log fi(mm(a,b)) )
+
+// a = 12, b = 18
+// HCF = 6
+
+// LCM = (12 × 18) / 6 = 36
+// Multiples of 4 → 4, 8, 12, 16
+// Multiples of 6 → 6, 12, 
+
+// ✅ LCM = 12
+
+// 🔹 Methods to Find LCM
+// 1️⃣ Listing Method (for small numbers)
+
+// List multiples of each number and find the first common one.
+
+// 2️⃣ Prime Factorization Method
+
+// Example: LCM of 12 and 18
+
+// 12 = 2² × 3
+// 18 = 2 × 3²
+
+// Take highest powers of each prime:
+// LCM = 2² × 3² = 36
+// a = 12, b = 18
+// HCF = 6
+
+// LCM = (12 × 18) / 6 = 36 a*b/HCF 
+
+
+function GCD(a,b) {
+
+  //  if( a = 1) return 1
+  //  if( b <= 1) return 1
+   while( a > 0 && b > 0) {
+    if(a>b) {
+      a = a % b
+    }
+    else {
+      b = b % a 
+    }
+
+   }
+
+    if ( a === 0) return b
+    if( b === 0) return a 
+}
+
+console.log("LCM" , (6*12)/GCD(6,12))
